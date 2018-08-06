e3e7cec535fb03bcd1518163be3e53bc340c38f7
VAUTRAIN Christophe <christophe.vautrain@ext.soprasteria.com>
2018-08-06 12:11:39 +0200

    Fix (Front-end): button to create a profile on home + route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,12 +1,23 @@
 import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// Guards
+import { AuthGuard } from './guards/auth.guard';
+
 // Components
 import { HomeComponent } from './components/home/home.component';
+import { ProfileFormComponent } from './components/profile/profile-form/profile-form.component';
 
 const ROUTES: Routes = [{
   path: 'home',
   component: HomeComponent
+}, {
+  path: 'profile',
+  canActivate: [AuthGuard],
+  children: [{
+    path: '',
+    component: ProfileFormComponent
+  }]
 }, {
   path: '**',
   redirectTo: 'home'
