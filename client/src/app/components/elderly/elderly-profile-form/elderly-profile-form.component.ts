24febeeb6467143e05771247368e598e6fe54f92
VAUTRAIN Christophe <christophe.vautrain@ext.soprasteria.com>
2018-08-06 15:11:13 +0200

    Add (Front-end): fields on elderly profile form

--- client/src/app/components/elderly/elderly-profile-form/elderly-profile-form.component.ts
+++ client/src/app/components/elderly/elderly-profile-form/elderly-profile-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ElderlyClass } from '../../../domain/elderly.class';
+import { CustomRegExp } from '../../../util/CustomRegExp';
 
 @Component({
   selector: 'app-elderly-profile-form',
@@ -8,19 +11,26 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class ElderlyProfileFormComponent implements OnInit {
 
+  public readonly today: Date = new Date();
   public profileForm: FormGroup;
+  private elderly: ElderlyClass;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.elderly = this.route.snapshot.data['user'] || new ElderlyClass();
     this.initForm();
   }
 
   private initForm() {
     this.profileForm = new FormGroup({
-      'firstname': new FormControl('', Validators.required),
-      'lastname': new FormControl('', Validators.required),
-      'birthdate': new FormControl('')
+      'firstname': new FormControl(this.elderly.firstname, Validators.required),
+      'lastname': new FormControl(this.elderly.lastname, Validators.required),
+      'birthdate': new FormControl(this.elderly.birthdate),
+      'address': new FormControl(this.elderly.address),
+      'postalCode': new FormControl(this.elderly.postalCode, Validators.pattern(CustomRegExp.ZIPCODE)),
+      'city': new FormControl(this.elderly.city),
+      'phone': new FormControl(this.elderly.phone, Validators.pattern(CustomRegExp.PHONE))
     });
   }
 
