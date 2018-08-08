381c9dc8520c3ea37de82a1dd58990439fbb79e8
VAUTRAIN Christophe <christophe.vautrain@ext.soprasteria.com>
2018-08-08 14:40:38 +0200

    Fix (Back-end): starters & dishes

--- server/scripts/z11-generate_starters.js
+++ server/scripts/z11-generate_starters.js
@@ -4,24 +4,39 @@ const entityUtils = require('../util/EntityUtils');
 
 module.exports = {
   generate: function(app) {
-    const starters = [
-      {name: 'Salade de tomates'},
-      {name: 'Tomates et concombres'},
-      {name: 'Radis roses'},
-      {name: 'Radis noirs'},
-      {name: 'Carottes rapées'},
-      {name: 'Asperges'},
-      {name: 'Coeurs d\'artichauds'},
-      {name: 'Charcuterie'},
-      {name: 'Betteraves rouges en dés'},
-      {name: 'Betteraves rouges en salade'},
-      {name: 'Choux blanc en salade'},
-      {name: 'Coeurs de palmiers'},
-      {name: 'Avocats'},
-      {name: 'Cake au saumon'},
-      {name: 'Tomates cerise'},
-      {name: 'Lasagnes aux épinards et chèvre'},
-    ];
+    const starters = [{
+      name: 'Salade de tomates',
+    }, {
+      name: 'Tomates et concombres',
+    }, {
+      name: 'Radis roses',
+    }, {
+      name: 'Radis noirs',
+    }, {
+      name: 'Carottes rapées',
+    }, {
+      name: 'Asperges',
+    }, {
+      name: 'Coeurs d\'artichauds',
+    }, {
+      name: 'Charcuterie',
+    }, {
+      name: 'Betteraves rouges en dés',
+    }, {
+      name: 'Betteraves rouges en salade',
+    }, {
+      name: 'Choux blanc en salade',
+    }, {
+      name: 'Coeurs de palmiers',
+    }, {
+      name: 'Avocats',
+    }, {
+      name: 'Cake au saumon',
+    }, {
+      name: 'Tomates cerise',
+    }, {
+      name: 'Lasagnes aux épinards et chèvre',
+    }];
 
     return entityUtils.createInOrder(app.models.starter, starters);
   },
--- server/scripts/z12-generate_dishes.js
+++ server/scripts/z12-generate_dishes.js
@@ -4,24 +4,39 @@ const entityUtils = require('../util/EntityUtils');
 
 module.exports = {
   generate: function(app) {
-    const dishes = [
-      {name: 'Côte de boeuf et haricots verts'},
-      {name: 'Escalope de poulet et courgettes'},
-      {name: 'Tomates farcies et riz'},
-      {name: 'Couscous'},
-      {name: 'Omelette au jambon'},
-      {name: 'Steak & frites'},
-      {name: 'Filet de saumon et haricots beurre'},
-      {name: 'Filet de truite aux amandes et patates'},
-      {name: 'Dos de cabillaud sur lit de poireau'},
-      {name: 'Escalope de dinde et poivrons'},
-      {name: 'Ratatouille et riz'},
-      {name: 'Penne aux champignons et noix'},
-      {name: 'Lasagne de potiron et tomates cerise'},
-      {name: 'Salade de haricots blancs et légumes grillés'},
-      {name: 'Paella au quinoa'},
-      {name: 'Salade grecque au tofu'},
-    ];
+    const dishes = [{
+      name: 'Côte de boeuf et haricots verts',
+    }, {
+      name: 'Escalope de poulet et courgettes',
+    }, {
+      name: 'Tomates farcies et riz',
+    }, {
+      name: 'Couscous',
+    }, {
+      name: 'Omelette au jambon',
+    }, {
+      name: 'Steak & frites',
+    }, {
+      name: 'Filet de saumon et haricots beurre',
+    }, {
+      name: 'Filet de truite aux amandes et patates',
+    }, {
+      name: 'Dos de cabillaud sur lit de poireau',
+    }, {
+      name: 'Escalope de dinde et poivrons',
+    }, {
+      name: 'Ratatouille et riz',
+    }, {
+      name: 'Penne aux champignons et noix',
+    }, {
+      name: 'Lasagne de potiron et tomates cerise',
+    }, {
+      name: 'Salade de haricots blancs et légumes grillés',
+    }, {
+      name: 'Paella au quinoa',
+    }, {
+      name: 'Salade grecque au tofu',
+    }];
 
     return entityUtils.createInOrder(app.models.dish, dishes);
   },
