cd2cf17ba833e284c8df2701570395ddf7581a15
VAUTRAIN Christophe <christophe.vautrain@ext.soprasteria.com>
2018-08-08 16:05:40 +0200

    Add (Back-end): endpoint to generate elderly's meals for a given period (associate starters & dishes)

--- server/models/elderly.js
+++ server/models/elderly.js
@@ -1,5 +1,46 @@
 'use strict';
 
-module.exports = function(Elderly) {
+const _ = require('lodash');
 
+module.exports = function(Elderly) {
+  /**
+   * Init meals in from today to endDate, by attributing some starters and dishes to each meal
+   *
+   * @param {number} elderlyId
+   * @param {Date} startDate
+   * @param {Date} endDate
+   */
+  Elderly.initMeals = async function(elderlyId, endDate) {
+    // Retrieve elderly's meals for the given period
+    const meals = await Elderly.app.models.meal.find({where: {
+      elderlyId: elderlyId,
+      and: [{date: {gte: new Date()}}, {date: {lte: endDate}}],
+    }});
+    // Do not compute anything if all meals are already linked to a starter and a dish
+    if (meals.every(meal => meal.starterId && meal.dishId)) {
+      return meals;
+    }
+    // Get all starters & dishes, and shuffle the lists (random)
+    let starters = await Elderly.app.models.starter.find();
+    starters = _.shuffle(starters);
+    let dishes = await Elderly.app.models.dish.find();
+    dishes = _.shuffle(dishes);
+    // Attribute each starter and dish to meals
+    for (let i = 0; i < meals.length; i++) {
+      await meals[i].starter(starters[i % starters.length]);
+      await meals[i].dish(dishes[i % dishes.length]);
+      await meals[i].save();
+    }
+    return meals;
+  };
+
+  Elderly.remoteMethod('initMeals', {
+    description: '[Custom] Init all meels for the given period',
+    accepts: [
+      {arg: 'id', type: 'number'},
+      {arg: 'endDate', type: 'date'},
+    ],
+    http: {path: '/:id/meals/init', verb: 'post'},
+    returns: {arg: 'meals', type: '[meal]', root: true},
+  });
 };
