b63fb94ceca91cb3f03c29cb8ef54a2ac2640556
jc950 <luquet.jc@gmail.com>
2018-08-21 14:47:53 +0200

    Fix docker config

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  baseUrl: 'https://int.lyve-lyon.alpha.grandlyon.com/liste-de-courses/api:3000',
+  baseUrl: 'https://int.lyve-lyon.alpha.grandlyon.com/api:3000',
   production: true
 };
