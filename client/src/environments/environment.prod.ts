722650da017f45d892e16a0b2daba553eeea5c58
Pierre-Gilles Levallois <pgl@erasme.org>
2018-09-10 16:00:26 +0200

    set baseURL to localhost

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  baseUrl: 'https://int.lyve-lyon.alpha.grandlyon.com',
+  // baseUrl: 'https://int.lyve-lyon.alpha.grandlyon.com',
+  baseUrl: 'http://localhost:8080/',
   production: true
 };
