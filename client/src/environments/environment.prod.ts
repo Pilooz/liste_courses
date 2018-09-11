59c03fb2d3e665bb03ee3826eb77c1b84f428761
root <root@docker.erasme.org>
2018-09-11 16:19:50 +0200

    corrects prod environnement

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
+  baseUrl: 'http://docker.erasme.org:8080/',
   // baseUrl: 'https://int.lyve-lyon.alpha.grandlyon.com',
-  baseUrl: 'http://localhost:8080/',
+  // baseUrl: 'http://localhost:8080/',
   production: true
 };
